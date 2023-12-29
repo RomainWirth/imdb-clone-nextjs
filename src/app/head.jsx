b305c6ee025c1bd4493dcf114f034536717e5782
Romain WIRTH <romain@89labs.io>
2023-12-29 17:29:37 +0100

    UPD app

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+        <>
+            <title>IMDb Clone</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <meta name="decription" content="This is an IMDb clone website" />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    )
+}
